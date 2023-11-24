82b6ba1a99611edd2c98baf952d1897d659759ca
Rehman Sohan <robin.shrkr@gmail.com>
2023-11-25 04:06:09 +0600

    schema model added

---
+++ src/app/modules/user/user.shemaModel.ts
@@ -0,0 +1,89 @@
+import { Schema, model } from 'mongoose';
+import { TAddress, TFullName, TOrder, TUser } from './user.interface';
+
+const orderSchema = new Schema<TOrder>({
+  productName: {
+    type: String,
+    required: [true, 'Product name is required'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+  },
+});
+
+const addressSchema = new Schema<TAddress>({
+  street: {
+    type: String,
+    required: [true, 'Street is required'],
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: [true, 'City is required'],
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: [true, 'Country is required'],
+    trim: true,
+  },
+});
+
+const fullNameSchema = new Schema<TFullName>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+    trim: true,
+  },
+});
+
+const userSchema = new Schema<TUser>({
+  userId: {
+    type: Number,
+    required: [true, 'User ID is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    select: false,
+  },
+  fullName: fullNameSchema,
+  age: {
+    type: Number,
+    required: [true, 'Age is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    trim: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: [true, 'isActive is required'],
+  },
+  hobbies: {
+    type: [String],
+    required: [true, 'Hobbies are required'],
+  },
+  address: addressSchema,
+  orders: [orderSchema],
+});
+
+export const UserModel = model<TUser>('User', userSchema);
