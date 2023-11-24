2b11257face4fca2ac136a71ce61a888c2eea149
Rehman Sohan <robin.shrkr@gmail.com>
2023-11-25 04:04:35 +0600

    config and interface file added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { UserRoutes } from './app/modules/user/user.route';
 const app = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/users', UserRoutes);
 app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-
-  res.send(a);
+  res.send(`App Running`);
 });
 
 export default app;
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,29 @@
+export type TOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type TAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type TFullName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: TFullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: Array<string>;
+  address: TAddress;
+  orders?: Array<TOrder>;
+};
